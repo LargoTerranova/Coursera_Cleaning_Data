96ca857a5884d2c39e465b76fc986b8102db513f
largoterranova@gmail.com <largoterranova@gmail.com>
2017-08-12 20:13:02 +0200

    Multiple changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,14 @@
 #######################################
-#Read in Data##########################
+#Working Directory#####################
 #######################################
 setwd("C:/Users/boband/Google Drive/DataScience/3_Cleaning_data/Assignement/wd/")
 
 
+
+
+#######################################
+#Read in Data##########################
+#######################################
 #Variable Names
 features        <- read.table("features.txt")
 
@@ -19,6 +24,7 @@ Y_test       <- read.table("y_test.txt")
 subject_train <- read.table("subject_train.txt")
 X_train       <- read.table("X_train.txt")
 Y_train       <- read.table("y_train.txt")
+
 
 
 
@@ -38,12 +44,12 @@ SUBJECT <- rbind(subject_train, subject_test)
 
 
 
+
 ######################################
 #Subsetting
 ######################################
 #Grep searches for the Variables containing "mean" and "std" and save their position into SELECTED_FEATURES
 SELECTED_FEATURES <- grep("-(mean|std)\\(\\)", features[, 2])
-
 
 #This creates the subsetted frame with the variables we want. Note that SELECTED_FEATURES is in the square 
 #brackets after the comma, column position, so we only fetch the columns we selected in the previous step
@@ -52,6 +58,7 @@ X_DATA <- X_DATA[, SELECTED_FEATURES]
 #Checking if we did everything right
 X_DATA[1:10,1:5]
 str(X_DATA)
+
 
 
 
@@ -67,6 +74,7 @@ names(X_DATA) <- features[SELECTED_FEATURES, 2]
 
 
 
+
 ######################################
 #Matching numbers and Activity
 ######################################
@@ -78,6 +86,9 @@ Y_DATA[,1] <- activitiez[Y_DATA[,1], 2]
 #Assign a proper column name
 names(Y_DATA) <- "activity"
 
+#Alternative Code which should do the same
+#train_test_frame$activity <- factor(train_test_frame$activity, levels = activity_labels[,1], labels = activity_labels[,2])
+
 
 
 
@@ -87,7 +98,6 @@ names(Y_DATA) <- "activity"
 #Assign a good column name
 SUBJECT
 names(SUBJECT) <- "subject"
-
 
 
 
@@ -106,14 +116,42 @@ MERGED[1:10,1:5]
 
 
 #######################################
+#Create Tidy variable names
+#######################################
+#There are strings in the variable names which make the variables unpractical like "()" and "-
+#Also there are abreviation like "t" for time, which might be confusing
+#This step corrects theese shortcommings. First check out the initial state
+MERGED[1:10, 1:5]
+
+#Correct Variable names
+names(MERGED) <- gsub("^f", "F_", names(MERGED))
+names(MERGED) <- gsub("^t", "T_", names(MERGED))
+names(MERGED) <- gsub("mean", "_MEAN", names(MERGED))
+names(MERGED) <- gsub("std", "_STD", names(MERGED))
+names(MERGED) <- gsub("BodyBody", "Body", names(MERGED))
+names(MERGED) <- gsub("\\(.*\\)", "", names(MERGED))
+names(MERGED) <- gsub("-", "", names(MERGED))
+
+names(MERGED) <- gsub("X$", "_X", names(MERGED))
+names(MERGED) <- gsub("Y$", "_Y", names(MERGED))
+names(MERGED) <- gsub("Z$", "_Z", names(MERGED))
+
+#Check out how cool the variables now look :)
+MERGED[1:10, 1:5]
+
+
+
+
+#######################################
 #Create Averages 
 #######################################
 library(plyr)
 AVERAGE <- ddply(MERGED, .(subject, activity), function(x) colMeans(x[,1:66]))
 
-
+#Check out the result
 AVERAGE[1:10,1:5]
 str(AVERAGE)
+
 
 
 
@@ -121,9 +159,12 @@ str(AVERAGE)
 #Write Table
 #######################################
 write.table(AVERAGE, "AVERAGE.txt")
+
+
+#Reload to inspect if everything was saved correctly
 FINAL <- read.table("AVERAGE.txt")
 str(FINAL)
-
+FINAL[1:10,1:5]
 
 
 
@@ -131,23 +172,3 @@ str(FINAL)
 
 #END OF CODE
 #######################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
